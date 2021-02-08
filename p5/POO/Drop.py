8dec2e96ee2cdb61bd20e552eb71b2e619001d14
demurgequentin <78725159+demurgequentin@users.noreply.github.com>
2021-02-08 11:54:37 +0100

    Add files via upload
    premier tp

---
+++ p5/POO/Drop.py
@@ -0,0 +1,25 @@
+import random
+
+import pygame
+from pygame.math import Vector2
+
+
+class Drop:
+    def __init__(self, largeur):
+        self.gravity = random.randint(5,10)
+        self.size = random.randint(5,10)
+        self.R = random.randint(0,255)
+        self.G = random.randint(0,255)
+        self.B = random.randint(0,255)
+        self.position = Vector2(random.randint(0,largeur), random.randint(-200,0))
+
+    def tomber(self, taillefenetre):
+        self.position.y = self.position.y + self.gravity
+        if self.position.y > taillefenetre:
+            self.raz()
+
+    def raz(self):
+        self.position.y = 0
+
+    def afficher(self, core):
+        pygame.draw.line(core.screen, (self.R, self.G, self.B), (self.position.x, self.position.y), (self.position.x, self.position.y + 10), 1)
---
+++ p5/POO/rainpoo.py
@@ -0,0 +1,42 @@
+import random
+import pygame
+from pygame.math import Vector2
+import core
+from POO.Drop import Drop
+
+drops = []
+
+def setup():
+    print("Setup START---------")
+    core.fps = 30
+    core.WINDOW_SIZE = [400, 400]
+
+    for i in range(0,1000):
+        drops.append(Drop(400))
+
+    print("Setup END-----------")
+
+
+def run():
+    for d in drops:
+        d.tomber(400)
+        d.afficher(core)
+
+
+
+
+def update():
+    print(core.keyPressValue)
+    for drop in drops:
+        drop.y += gravity
+        if drop.y > core.WINDOW_SIZE[1]:
+            drop.x = random.randint(0, core.WINDOW_SIZE[0])
+            drop.y = random.randint(-400,0)
+
+
+def show():
+    for drop in drops:
+        pygame.draw.line(core.screen, (255, 0, 0), (drop.x, drop.y), (drop.x, drop.y + 10), 1)
+
+
+core.main(setup, run)
